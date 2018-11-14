772c5861f3f389408021214ec406c40cf7c71bb1
Krzystof Kensicki <kensickik@gmail.com>
2018-11-14 02:30:20 +0100

    1.1

---
+++ Figure/One/One.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inwazja.Figure.One
+{
+    public sealed class One : Figure
+    {
+        public One(FigureDefinition definition) : base(definition)
+        {
+            Moves = Moves.Distinct().ToList();
+        }
+
+        protected override List<Tuple<int, int>> GetValidTurns()
+        {
+            return null;
+        }
+    }
+}
