90b9ce3d514b80305a6fb10c112371fda2e80c3c
Kens97 <kensickik@gmail.com>
2018-12-31 01:09:24 +0100

    1.2

--- Start.cs
+++ Start.cs
@@ -13,7 +13,7 @@ namespace Inwazja
 {
     public partial class Start : Form
     {
-       // private Figure figura = new Figure();
+        // private Figure figura = new Figure();
 
         private const int tileWidth = 20;
         private const int tileHeight = 20;
@@ -21,18 +21,19 @@ namespace Inwazja
         private const int MarginWidth = 40;
         private const int MarginHeigt = 40;
 
-        private const int FigureHolderWidth = 22*tileWidth ;
-        private const int FigureHolderHeight =13*tileHeight;
+        private const int FigureHolderWidth = 22 * tileWidth;
+        private const int FigureHolderHeight = 13 * tileHeight;
 
-        public const int size= 20;
+        public const int size = 20;
         private static readonly Size tileSize = new Size(tileWidth, tileHeight);
         private static readonly Size FigureHolderSize = new Size(FigureHolderWidth, FigureHolderHeight);
 
 
-        private Point tileLocation = new Point(tileSize.Width/2, tileSize.Height/2);
-        private Point FigureHolderLocation = new Point(MarginWidth*2 + tileWidth*size+ FigureHolderWidth/2, MarginHeigt + FigureHolderHeight/2);
-        public PictureBox[][] Board;
-        public PictureBox[][] FigureHolder;
+        private Point tileLocation = new Point(tileSize.Width / 2, tileSize.Height / 2);
+        private Point FigureHolderLocation = new Point(MarginWidth * 2 + tileWidth * size + FigureHolderWidth / 2, MarginHeigt + FigureHolderHeight / 2);
+        public PictureBox[,] Board;
+        public PictureBox[,] FigureHolder;
+        public PictureBox picture;
 
         //static List<Figure> Figures = new List<Figure>();
 
@@ -41,59 +42,62 @@ namespace Inwazja
             InitializeComponent();
             DrawBoard();
             DrawFigureHolder();
+            Draw_Picturbox();
             DrawCentralTile();
         }
 
         private void DrawBoard()
         {
+            Board = new PictureBox[size, size];
 
-
-            PictureBox[][] Board = new PictureBox[size][];
             for (int i = 0; i < size; i++)
             {
-                Board[i] = new PictureBox[size];
-            }
-
-
-            for(int i=0; i<size; i++)
-            {
-                for(int j=0; j<size; j++)
+                for (int j = 0; j < size; j++)
                 {
-                    Board[i][j] = new PictureBox
+                    Board[i, j] = new PictureBox
                     {
                         Size = tileSize,
-                        Location = new Point(MarginWidth + i*tileWidth, MarginHeigt + j*tileHeight),
-                        BorderStyle = BorderStyle.Fixed3D,
-                        BackColor=Color.Black,
-                        SizeMode=PictureBoxSizeMode.StretchImage
+                        Location = new Point(MarginWidth + i * tileWidth, MarginHeigt + j * tileHeight),
+                        BorderStyle = BorderStyle.None,
+                        BackColor = Color.Black,
+                        SizeMode = PictureBoxSizeMode.StretchImage
                     };
-                    Controls.Add(Board[i][j]);
-                    
+                    Controls.Add(Board[i, j]);
+
                 }
             }
         }
 
-        private void DrawFigureHolder ()
+        private void Draw_Picturbox()
         {
-            PictureBox[][] FigureHolder = new PictureBox[22][];
-            for (int i = 0; i < 22; i++)
+            picture = new PictureBox
             {
-                FigureHolder[i] = new PictureBox[13];
-            }
+                Size = tileSize,
+                Location = new Point(600,600),
+                BorderStyle = BorderStyle.None,
+                BackColor = Color.Blue,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            Controls.Add(picture);
+        }
 
+
+        private void DrawFigureHolder()
+        {
+            FigureHolder = new PictureBox[22, 13];
 
             for (int i = 0; i < 22; i++)
             {
                 for (int j = 0; j < 13; j++)
                 {
-                    FigureHolder[i][j] = new PictureBox
+                    FigureHolder[i, j] = new PictureBox
                     {
                         Size = tileSize,
-                        Location = new Point(MarginWidth*2 + 20*tileWidth + i * tileWidth, MarginHeigt*2 + tileHeight + j * tileHeight),
+                        Location = new Point(MarginWidth * 2 + 20 * tileWidth + i * tileWidth, MarginHeigt * 2 + tileHeight + j * tileHeight),
                         BorderStyle = BorderStyle.Fixed3D,
                         BackColor = Color.Green
                     };
-                    Controls.Add(FigureHolder[i][j]);
+                    Controls.Add(FigureHolder[i, j]);
 
                 }
             }
@@ -101,7 +105,33 @@ namespace Inwazja
 
         private void DrawCentralTile()
         {
-            Board[1][1].Image = Image.FromFile(ImagePath.YellowImagePath);
+            picture.Image = Image.FromFile(ImagePath.YellowImagePath);
+        }
+
+        public bool moveDetect = false;
+
+        private void picture_MouseDown(object sender, MouseEventArgs e)
+        {
+            moveDetect = true;
+        }
+
+        private void picture_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moveDetect)
+            {
+                Point pozycja = new Point();
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                pozycja = this.PointToClient(Cursor.Position);
+                picture.Location = pozycja;
+            }
+        }
+
+        private void picture_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (moveDetect)
+            {
+                moveDetect = false;
+            }
         }
     }
 }
